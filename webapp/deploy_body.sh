48a2ace25380710772e65adedab1b1597e188ef4
catatsuy <catatsuy@catatsuy.org>
2018-10-20 12:03:11 +0900

    rsync ignore

--- webapp/deploy_body.sh
+++ webapp/deploy_body.sh
@@ -3,7 +3,7 @@
 echo "start deploy ${USER}"
 for server in g1-11 g2-11 g3-11 g4-11; do
     ssh -t $server "sudo systemctl stop isucoin"
-    rsync -av ./go/src $server:/home/isucon/isucon2018-final/webapp/go/src/
+    rsync -av  --exclude-from=rsyncignore ./go/src/ $server:/home/isucon/isucon2018-final/webapp/go/src/
     ssh -t $server "sudo systemctl start isucoin"
 done
 
